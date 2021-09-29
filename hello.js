e2d105b5d718053df408c041a312223c70bd1afe
Jarkko Vallius <jarkko.vallius@gmail.com>
2021-09-29 21:52:44 +0300

    update skeleton

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 let skeleton = [
-  "             ▄▐",
+  "   hello!    ▄▐",
   "       ▄▄▄  ▄██▄",
   "      ▐▀█▀▌    ▀█▄ ",
   "      ▐█▄█▌      ▀█▄",
@@ -47,18 +47,22 @@ function handleChar(c) {
 
 
 let step = 0
+const DIVIDER = 0.1
+const INDEX_DIVIDER = 20
+const STEP_INCREASE = 1
+const INTERVAL = 100
 
 setInterval(() => {
   let rows = skeleton.map(e => [...e].map(c => handleChar(c)).join(""))
 
-  const blurValue = Math.sin(step / 10) * 10
+  const blurValue = Math.sin(step / 10) * 20
 
   const blur = blurValue > 0 ? blurValue : 1
 
   // console.log("blur", blur)
 
   rows = rows.map((row, index) => {
-    const val = Math.abs(Math.floor(Math.sin((index + step) / 2) * blur))
+    const val = Math.abs(Math.floor(Math.sin((index / INDEX_DIVIDER + step) / DIVIDER) * blur))
     // console.log("index ", index, val)
     const movement = handleChar(' ').repeat(val)
     return movement + row
@@ -66,8 +70,8 @@ setInterval(() => {
 
   html = (rows.map(r => r + "<br />")).join("")
   document.getElementById('main').innerHTML = html
-  step = step + 1
-}, 100)
+  step = step + STEP_INCREASE
+}, INTERVAL)
 
 
 
